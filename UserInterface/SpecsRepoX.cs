8cad1ed65a3d444ecef313b557e26b68ec68f9a8
TaherMarengoze <taher.marengoze@gmail.com>
2021-07-27 01:03:11 +0200

    Specs Modify
    Add implementation for Interface

---
+++ UserInterface/SpecsRepoX.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserInterface
+{
+    using Interfaces;
+    using Models;
+
+    public class SpecsRepoX : ISpecsModifier
+    {
+        private XElement SerializeSpecs(Specs specs)
+        {
+            //XElement specItem;
+            //XElement speclist;
+            XElement draftSpecs =
+                new XElement("specs",
+                new XAttribute("specsID", specs.ID),
+                new XAttribute("name", specs.Name),
+                new XAttribute("textPattern", specs.TextPattern));
+
+            foreach (Spec spec in specs.SpecItems)
+            {
+                XElement specItem =
+                    new XElement("specsItem",
+                    new XAttribute("index", spec.Index),
+                    new XAttribute("name", spec.Name),
+                    new XAttribute("valuePattern", spec.ValuePattern));
+
+                if (spec.SpecType == Enums.SpecType.List)
+                {
+                    XElement speclist = new XElement("list");
+
+                    foreach (SpecListEntry entry in spec.ListEntries)
+                    {
+                        speclist.Add(
+                            new XElement("entry",
+                            new XAttribute("valId", entry.ValueID),
+                            new XElement("val") { Value = entry.Value },
+                            new XElement("disp") { Value = entry.Display }));
+                    }
+
+                    specItem.Add(speclist);
+                }
+                else
+                {
+                    specItem.Add(new XElement("custom") { Value = spec.CustomInputID });
+                }
+
+                draftSpecs.Add(specItem);
+            }
+
+            return draftSpecs;
+        }
+    }
+}
