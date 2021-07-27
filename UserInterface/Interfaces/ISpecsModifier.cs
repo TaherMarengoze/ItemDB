4257a18835cdc3225c1f93ed4a287af03ba37ad0
TaherMarengoze <taher.marengoze@gmail.com>
2021-07-27 17:48:32 +0200

    Documenting
    Add documentation for the interface (ISpecsModifier.cs)

--- UserInterface/Interfaces/ISpecsModifier.cs
+++ UserInterface/Interfaces/ISpecsModifier.cs
@@ -8,10 +8,23 @@ namespace UserInterface.Interfaces
 {
     public interface ISpecsModifier
     {
+        /// <summary>
+        /// Adds a new specs to a data source
+        /// </summary>
+        /// <param name="specs"></param>
         void AddSpecs(Models.Specs specs);
 
+        /// <summary>
+        /// Modifies an existing specs in the data source
+        /// </summary>
+        /// <param name="refId"></param>
+        /// <param name="specs"></param>
         void ModifySpecs(string refId, Models.Specs specs);
 
+        /// <summary>
+        /// Deletes a specs from its data source
+        /// </summary>
+        /// <param name="specsId">The delete specs ID</param>
         void DeleteSpecs(string specsId);
     }
 }
