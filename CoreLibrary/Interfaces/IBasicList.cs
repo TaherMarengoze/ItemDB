dfad7b14228a8b721f9ff0ba3b0d63ebedbe0e7d
TaherMarengoze <taher.marengoze@gmail.com>
2021-08-19 17:05:19 +0200

    Add New Project
    Add new Class Library project.
    Add needed classed for separation of DataService class.

---
+++ CoreLibrary/Interfaces/IBasicList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CoreLibrary.Interfaces
+{
+    public interface IBasicList
+    {
+        string ID { get; set; }
+        ObservableCollection<string> List { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ CoreLibrary/Models/SpecListEntry.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreLibrary.Models
+{
+    using Interfaces;
+
+    public class SpecListEntry : ISpecListEntry
+    {
+        public int ValueID { get; set; }
+        public string Value { get; set; }
+        public string Display { get; set; }
+
+        public ISpecListEntry CopyEntry()
+        {
+            return
+                new SpecListEntry()
+                {
+                    ValueID = ValueID,
+                    Value = Value,
+                    Display = Display
+                };
+        }
+    }
+
+    
+}
