6c8b2701b4ec9b8a71b4bf7b00d7aefd17ac50af
TaherMarengoze <taher.marengoze@gmail.com>
2021-08-25 17:58:39 +0200

    Update IItemView.cs

--- CoreLibrary/Interfaces/IItemView.cs
+++ CoreLibrary/Interfaces/IItemView.cs
@@ -3,10 +3,15 @@
     public interface IItemView
     {
         string Category { get; set; }
+
         string CatID { get; set; }
+
         string ID { get; set; }
+
         int Images { get; set; }
+
         string Name { get; set; }
+
         string UoM { get; set; }
     }
 }
