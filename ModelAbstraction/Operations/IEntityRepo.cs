3d01f34ac1bb0effef0215af53561fe5440bcbbe
TaherMarengoze <taher.marengoze@gmail.com>
2021-09-01 00:36:32 +0200

    Introduce Generics

---
+++ ModelAbstraction/Operations/IEntityRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Operations
+{
+    public interface IEntityRepo<TEntity>
+    {
+        void Create(TEntity group);
+
+        TEntity Read();
+
+        void Update(string refId, TEntity group);
+
+        void Delete(string groupId);
+    }
+}
---
+++ XmlDataSource/Repository/Specs.cs
@@ -0,0 +1,19 @@
+﻿
+using Interfaces.Models;
+using Interfaces.Operations;
+using System;
+
+
+namespace Modeling
+{
+    public class Specs<ISpecs> : IEntityRepo<ISpecs>
+    {
+        public void Create(ISpecs specs) => throw new NotImplementedException();
+
+        public ISpecs Read() => throw new NotImplementedException();
+
+        public void Update(string refId, ISpecs specs) => throw new NotImplementedException();
+
+        public void Delete(string specsId) => throw new NotImplementedException();
+    }
+}
