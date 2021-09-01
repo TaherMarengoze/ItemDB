79809e1d0658f1b1dbf6221ad844feb09129b755
TaherMarengoze <taher.marengoze@gmail.com>
2021-09-01 17:54:33 +0200

    SizeGroup Implementation

---
+++ XmlDataSource/Repository/SizeGroup.cs
@@ -0,0 +1,18 @@
+﻿
+using Interfaces.Models;
+using Interfaces.Operations;
+using System;
+
+namespace XmlDataSource.Repository
+{
+    public class SizeGroup : IEntityRepo<ISizeGroup>
+    {
+        public void Create(ISizeGroup entity) => throw new NotImplementedException();
+
+        public ISizeGroup Read() => throw new NotImplementedException();
+
+        public void Update(string refId, ISizeGroup entity) => throw new NotImplementedException();
+
+        public void Delete(string entityId) => throw new NotImplementedException();
+    }
+}
