63ae57420bdffa479ede45618ad0c3ff2af80a68
TaherMarengoze <taher.marengoze@gmail.com>
2022-01-10 18:01:27 +0200

    Create EventArgs

---
+++ Drafting/EventArgs/PreDraftingEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Interfaces.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Controllers
+{
+    public class PreDraftingEventArgs : EventArgs
+    {
+        public List<string> PreList { get; set; }
+
+        public IFieldList DraftObject { get; set; }
+    }
+}
---
+++ Drafting/EventArgs/SetEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Controllers
+{
+    public class SetEventArgs
+    {
+        public string SetID { get; set; }
+
+        public object NewList { get; set; }
+    }
+}
