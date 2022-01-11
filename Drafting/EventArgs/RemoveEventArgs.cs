0ad39e9b0c4c42bcd78b62c5fca43792ceedebb5
TaherMarengoze <taher.marengoze@gmail.com>
2022-01-11 18:00:35 +0200

    Add Class File
    Add Drafting\EventArgs\RemoveEventArgs.cs
    to Drafting project

---
+++ Drafting/EventArgs/RemoveEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class RemoveEventArgs
+    {
+        public string RemoveID { get; set; }
+
+        public object NewList { get; set; }
+
+        public int Count { get; set; }
+    }
+}
